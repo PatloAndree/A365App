163a6d66b561f31d56c4dfa9f21313be99043f89
Percy Torres <torresandree25@outlook.com>
2022-01-07 23:18:05 -0500

    Subiendo prueba - Fix

---
+++ app/src/main/java/com/example/a365app/RegisterActivity.java
@@ -0,0 +1,33 @@
+package com.example.a365app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        changeStatusBarColor();
+
+    }
+
+    private void changeStatusBarColor() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        }
+    }
+    public void onLoginClick(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+        overridePendingTransition(R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+}
